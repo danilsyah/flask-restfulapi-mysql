639c16416cf2f3b4f4dd6a92be6ffa70c83a7e67
danilsyah <danilsyaharihardjo@gmail.com>
2022-03-14 06:52:33 +0700

    upload project

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from app.controller import DosenController
+
+@app.route('/')
+def index():
+    return 'Hello Flask App'
+
+
+@app.route('/dosen', methods=['GET'])
+def dosens():
+    return DosenController.index()
+
